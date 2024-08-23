b761d8f4d003c74e1849c6937b69df4d3f69e9b8
Tomáš Križák <tomaskrizak@gmail.com>
2024-08-23 16:56:32 +0200

    setup starter html, css and javascript to generate gameboard

---
+++ tic-tac-toe.js
@@ -0,0 +1,48 @@
+const Gameboard = (() => {
+  let gameboard = ['', '', '', '', '', '', '', '', ''];
+
+  const render = () => {
+    const gameBoardElement = document.querySelector('#gameboard');
+    let boardHTML = '';
+
+    gameboard.forEach((square, index) => {
+      boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
+    });
+
+    gameBoardElement.innerHTML = boardHTML;
+  };
+
+  return { render };
+})();
+
+const createPlayer = (name, mark) => {
+  return {
+    name,
+    mark,
+  };
+};
+
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver;
+
+  const start = () => {
+    players = [
+      createPlayer(document.querySelector('#player1').value, 'X'),
+      createPlayer(document.querySelector('#player2').value, 'O'),
+    ];
+
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+    console.log(players);
+  };
+
+  return { start };
+})();
+
+const startButton = document.querySelector('#start-button');
+startButton.addEventListener('click', () => {
+  Game.start();
+});
